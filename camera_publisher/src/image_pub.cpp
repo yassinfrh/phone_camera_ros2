3267310e59d71816a71ff42a067d9e9c3cbb7c80
Yassin Farah <yassin.frh@gmail.com>
2024-07-29 16:46:52 +0200

    ask user for camera id

--- camera_publisher/src/image_pub.cpp
+++ camera_publisher/src/image_pub.cpp
@@ -12,7 +12,7 @@
 class ImagePublisher : public rclcpp::Node
 {
 public:
-    ImagePublisher() : Node("image_publisher"), count_(0)
+    ImagePublisher(int camera_id) : Node("image_publisher"), count_(0), camera_id_(camera_id)
     {
         // Publisher for the image
         publisher_ = this->create_publisher<sensor_msgs::msg::Image>("/android_camera/image", 10);
@@ -20,13 +20,13 @@ public:
         // Timer to publish the image
         timer_ = this->create_wall_timer(std::chrono::milliseconds(100), std::bind(&ImagePublisher::publish_image, this));
     }
-    
+
 private:
     // Callback function to publish the image
     void publish_image()
     {
-        // Create capture from camera 2 (change later)
-        cv::VideoCapture cap(0);
+        // Create capture from camera_id
+        cv::VideoCapture cap(camera_id_);
 
         // Check if the camera is opened
         if (!cap.isOpened())
@@ -53,20 +53,36 @@ private:
         cap.release();
     }
 
-    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;   // Publisher for the image
-    rclcpp::TimerBase::SharedPtr timer_;                                // Timer to publish the image
-    sensor_msgs::msg::Image::SharedPtr msg_;                            // Message to store the image
-    size_t count_;                                                      // Counter for the image                                          
-
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_; // Publisher for the image
+    rclcpp::TimerBase::SharedPtr timer_;                              // Timer to publish the image
+    sensor_msgs::msg::Image::SharedPtr msg_;                          // Message to store the image
+    size_t count_;                                                    // Counter for the image
+    int camera_id_;                                                   // Camera ID
 };
 
 int main(int argc, char *argv[])
 {
     // Initialize ROS
     rclcpp::init(argc, argv);
-    
+
+    // Ask the user for the camera ID
+    int camera_id;
+    while (true)
+    {
+        std::cout << "Enter the camera ID: ";
+        std::cin >> camera_id;
+
+        // Check if the camera ID is valid
+        cv::VideoCapture cap(camera_id);
+        if (cap.isOpened())
+        {
+            cap.release();
+            break;
+        }
+    }
+
     // Create the node
-    auto node = std::make_shared<ImagePublisher>();
+    auto node = std::make_shared<ImagePublisher>(camera_id);
 
     // Spin the node
     rclcpp::spin(node);
